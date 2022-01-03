8ae927076dd3328e8160783e81d9ae881a8852f5
stanislavBelinskyi <belinskyi@qarea.us>
2022-01-03 22:29:11 +0200

    fix styles modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
@@ -7,7 +9,7 @@ import { InvestmentModule } from '@ui-modules/investment/investment.module'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, InvestmentModule],
+  imports: [BrowserModule, AppRoutingModule, InvestmentModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
 })
