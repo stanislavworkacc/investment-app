f1fcef5afd558ba31ee44f06937d05c63e494e07
stanislavBelinskyi <belinskyi@qarea.us>
2022-01-04 01:55:41 +0200

    delete console.log

--- src/app/shared/utils/common-utils.ts
+++ src/app/shared/utils/common-utils.ts
@@ -1,5 +1,4 @@
 export const isQueryParamNumber = (value: string): number => {
   const isNumber = Number(value);
-  console.log('isNumber', isNumber)
   return isNumber || isNumber === 0 ? isNumber : -1;
 }
